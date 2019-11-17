517ab0f9913bf7ca6996217e3158cfce184ffd99
Andrei Serban <serban-andrei@hotmail.com>
2019-11-17 15:09:07 -0500

    Completed Chapter 3 Problem 3

---
+++ Starting out with Java/Ch3/src/problem3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class problem3
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your weight in lbs");
+        double weightInput = keyboard.nextDouble();
+
+        while(weightInput < 40 || weightInput > 500)
+        {
+            System.out.println("Invalid weight entered");
+            System.out.println("Enter your weight in lbs");
+            weightInput = keyboard.nextDouble();
+        }
+        System.out.println("Enter your height in inches");
+        double heightInput = keyboard.nextDouble();
+        while(heightInput < 20 || heightInput > 160)
+        {
+            System.out.println("Invalid height entered");
+            System.out.println("Enter your height in inches");
+            heightInput = keyboard.nextDouble();
+        }
+        System.out.println("BMI = weight * 703 / height^2");
+        double BMI = weightInput * 703/(Math.pow(heightInput,2));
+        if(BMI < 18.5)
+        {
+            System.out.println("You are underweight");
+            System.out.println("Your BMI is " + BMI);
+        }
+        if(BMI > 25)
+        {
+            System.out.println("You are overweight");
+            System.out.println("Your BMI is " + BMI);
+        }
+        if(BMI >= 18.5 && BMI <= 25)
+        {
+            System.out.println("Your weight is optimal");
+            System.out.println("Your BMI is " + BMI);
+        }
+    }
+}
