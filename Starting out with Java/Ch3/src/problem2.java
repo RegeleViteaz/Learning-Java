f4abcf863852a5db5bb690cc6b8762b7b244794d
Andrei Serban <serban-andrei@hotmail.com>
2019-11-17 14:45:39 -0500

    Completed Chapter 3 Problem 2

--- Starting out with Java/Ch3/src/problem2.java
+++ Starting out with Java/Ch3/src/problem2.java
@@ -1,3 +1,46 @@
+import java.util.Scanner;
+
 public class problem2
 {
+    public static void main(String[]args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a month in numeric form (1 to 12)");
+        int monthInput = keyboard.nextInt();
+        while(monthInput < 1 || monthInput > 12)
+        {
+            System.out.println("Invalid month entered.");
+            System.out.println("Enter a month in numeric form (1 to 12)");
+            monthInput = keyboard.nextInt();
+        }
+
+        System.out.println("Enter a day in numeric form");
+        int dayInput = keyboard.nextInt();
+        while(dayInput < 1 || dayInput > 32)
+        {
+            System.out.println("Invalid day entered.");
+            System.out.println("Enter a day in numeric form");
+            dayInput = keyboard.nextInt();
+        }
+
+        System.out.println("Enter a two digit year");
+        int inputYear = keyboard.nextInt();
+        while(inputYear < 1 || inputYear > 99)
+        {
+            System.out.println("Invalid year entered.");
+            System.out.println("Enter a two digit year");
+            inputYear = keyboard.nextInt();
+        }
+
+        int dayMonth = dayInput * monthInput;
+        if(dayMonth != inputYear)
+        {
+            System.out.println("The date is not magic");
+        }
+        else
+        {
+            System.out.println("The date is magic");
+        }
+    }
 }
+
