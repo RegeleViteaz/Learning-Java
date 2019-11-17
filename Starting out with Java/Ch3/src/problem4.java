460596e38bca7906f4666f159f67ac3aa5868ab4
Andrei Serban <serban-andrei@hotmail.com>
2019-11-17 15:14:28 -0500

    Completed Chapter 3 Problem 3

--- Starting out with Java/Ch3/src/problem4.java
+++ Starting out with Java/Ch3/src/problem4.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+/**
+ * Date: 17/11/2019
+ * Starting out with Java Programming Challenge Chapter 3 Problem 4
+ * User enters a day, a month and a year in numeric form. It then determines if these values are magic.
+ */
 public class problem4
 {
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Welcome to your test score and grade calcuation");
+        System.out.println("Welcome to your test score and grade calculation");
     }
 }
