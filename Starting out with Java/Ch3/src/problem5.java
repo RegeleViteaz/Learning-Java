d3b0b44db61b3463acff97c261c78e47de9c4e36
Andrei Serban <serban-andrei@hotmail.com>
2019-11-17 15:57:51 -0500

    Completed Chapter 3 Problem 5

---
+++ Starting out with Java/Ch3/src/problem5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/**
+ * Date: 17/11/2019
+ * Starting out with Java Programming Challenge Chapter 3 Problem 5
+ * User enters an object's mass, calculates in weight in newtons and displays an appropriate message
+ */
+public class problem5
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the object's mass in kg");
+        double inputMass = keyboard.nextDouble();
+        double weight = inputMass * 9.8;
+        if(weight > 1000)
+            System.out.println("Object is too heavy, its weight is " + weight + " Newtons.");
+        if(weight < 10)
+            System.out.println("Object is too light, its weight is " + weight + " Newtons.");
+    }
+}
