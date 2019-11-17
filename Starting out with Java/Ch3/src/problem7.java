9549abf8cfa060ec53a0ded053414cb51c9a25b1
Andrei Serban <serban-andrei@hotmail.com>
2019-11-17 17:23:42 -0500

    In process of completing Chapter 3 Problem 7

---
+++ Starting out with Java/Ch3/src/problem7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Date: 17/11/2019
+ * Starting out with Java Programming Challenge Chapter 3 Problem 7
+ * User enters an amount of seconds and displays its equivalent in minutes, hours and days
+ */
+public class problem7
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter person 1 name");
+        String name1 = keyboard.nextLine();
+        System.out.println("Enter person 2 name");
+        String name2 = keyboard.nextLine();
+        System.out.println("Enter person 3 name");
+        String name3 = keyboard.nextLine();
+        
+    }
+}
