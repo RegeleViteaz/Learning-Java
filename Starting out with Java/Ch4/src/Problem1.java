1584c437d04dc2a3cba19561f9d13a1fb78238f8
Andrei Serban <serban-andrei@hotmail.com>
2020-01-06 18:06:13 -0500

    Completed Chapter 4 Problem 1

--- Starting out with Java/Ch4/src/Problem1.java
+++ Starting out with Java/Ch4/src/Problem1.java
@@ -2,11 +2,36 @@ import java.util.Scanner;
 
 /**
  * Date: 01/06/2020
- * Starting out with Java Programming Challenge Chapter 4 Problem 1
- * User enters a number between 1 and 5 and displays that number in Roman Numeral
+ * Starting out with Java Programming Challenge Chapter 4 Problem 1: Sum of Numbers
+ * Program that asks the user for a positive nonzero integer value and find its factorial.
  */
 
 public class Problem1
 {
+    public static void main(String[] args)
+    {
+         int totalSum = 0;
 
+        Scanner keyboard = new Scanner(System.in);
+        String msg = "Enter a positive nonzero integer value ";
+        System.out.print(msg);
+        int inputValue = keyboard.nextInt();
+
+        while(inputValue < 0)
+        {
+            System.out.println("You did not enter a positive nonzero integer value");
+            System.out.println(msg);
+            inputValue = keyboard.nextInt();
+        }
+
+        for(int i=1; i <= inputValue; i++)
+        {
+            if(i != inputValue)
+                System.out.print(i + " + ");
+            else
+                System.out.print(i);
+            totalSum += i;
+        }
+        System.out.print(" = " + totalSum);
+    }
 }
