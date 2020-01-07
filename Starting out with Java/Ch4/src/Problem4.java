c5344c61ac541a8832b44bb13ac0493fd4ea23bd
Andrei Serban <serban-andrei@hotmail.com>
2020-01-06 19:02:41 -0500

    Started Chapter 4 Problem 4

---
+++ Starting out with Java/Ch4/src/Problem4.java
@@ -0,0 +1,13 @@
+
+/**
+ * Date: 01/06/2020
+ * Starting out with Java Programming Challenge Chapter 4 Problem 4: Pennies for pay
+ * Program that calculates the amount a person would earn over a period of time
+ */
+public class Problem4
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
